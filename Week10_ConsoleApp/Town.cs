6c5b0ea80f6564c5359b3333a002a324fcd6b540
DerekPeacock <derek.peacock@yahoo.com>
2018-12-02 09:57:51 +0000

    Added the first task Unit 1

--- Week10_ConsoleApp/Town.cs
+++ Week10_ConsoleApp/Town.cs
@@ -6,11 +6,26 @@ using System.Threading.Tasks;
 
 namespace Week10_ConsoleApp
 {
+    /// <summary>
+    /// This class prompts the user to enter their
+    /// name and town, and then comments on their entries
+    /// 
+    /// CO452 Unit 1: Task 1
+    /// 02/12/2018 by Derek Peacock
+    /// </summary>
     class Town
     {
+        private static string town;
+        
         public static void Check()
         {
-            Console.WriteLine("Enter your town >");
+            Console.Clear();
+
+            Console.WriteLine("Task 1: Hello There!");
+            Console.Write("Enter the town you live in >");
+
+            town = Console.ReadLine();
+            Console.WriteLine(town + " is a nice town!");
         }
     }
 }
